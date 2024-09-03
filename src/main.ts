b1373cfb58c1dbe9a0f5b3fa925246ae92cea842
IamDalemark <danielweg123@gmail.com>
2024-09-03 13:15:23 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,48 @@
+import './style.css'
+const placehold = document.querySelector('#input') as HTMLInputElement
+placehold.placeholder = "Enter your Task";
+
+function Task(){
+  document.querySelector("#add_task")!.addEventListener("click", () =>{
+    // add task
+    const item_name = document.querySelector('#input') as HTMLInputElement;
+    const value = item_name?.value.trim();
+    
+    if (value === '') return
+    const node = document.createElement("li");
+    const textnode = document.createTextNode(value);
+    const value_no_space = value.replace(/\s+/g, ''); // removes spaces between words
+
+    //mark task complete
+    node.addEventListener('click', () =>{
+      node.setAttribute("class", "complete")
+    })
+
+    // add text
+    node.appendChild(textnode);
+
+
+    // delete button
+    const button = document.createElement("button")
+    button.setAttribute('class', 'delete_button')
+    node.setAttribute("id", value_no_space); // li id
+    node.setAttribute("class", 'items')
+    node.appendChild(button);
+
+
+    //remove task
+    button.addEventListener("click", () => {
+
+      document.querySelector(`#${value_no_space}`)?.remove()
+
+    })
+    // add to ul
+    document.querySelector("#cart")!.appendChild(node);
+    item_name!.value = ''
+  })
+}
+
+
+
+Task()
+
